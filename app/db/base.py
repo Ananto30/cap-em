6464ff7a239f0f651b73ca8adae04d8e862e3f6b
Ananto30 <azizulhaq.ananto@gmail.com>
2020-06-04 15:10:19 +0600

    Change docker build args and move that to run env

--- app/db/base.py
+++ app/db/base.py
@@ -4,9 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-db_url = os.getenv('DB_URL', 'sqlite:///capem.db')
+db_uri = os.getenv('DB_URI', 'sqlite:///capem.db')
 
-engine = create_engine(db_url)
+engine = create_engine(db_uri)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
