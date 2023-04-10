5e27b3f4da924642be32f0f56ccfb6376d718fd9
Azizul Haque Ananto <azizulhaq.ananto@gmail.com>
2023-04-10 13:16:05 +0200

    Fix tests

--- tests/service_test.py
+++ tests/service_test.py
@@ -1,8 +1,10 @@
 import time
 
+PREFIX = "/api/v1"
+
 
 def test_get_config(client):
-    response = client.simulate_get("/configs")
+    response = client.simulate_get(f"{PREFIX}/configs")
     assert response.status == "200 OK"
     print(response.text)
     assert (
@@ -22,20 +24,20 @@ call:
 def test_get_help(client):
     from src.api.rest import Help  # pylint: disable=import-outside-toplevel
 
-    response = client.simulate_get("/help")
+    response = client.simulate_get(f"{PREFIX}/help")
     assert response.status == "200 OK"
     assert response.text == Help.HELP_TEXT
 
 
 def test_usage_no_access_id(client):
-    response = client.simulate_get("/usage/email")
+    response = client.simulate_get(f"{PREFIX}/usage/email")
     assert response.status == "401 Unauthorized"
     assert response.json == {
         "error": "Unauthorized",
         "message": "Access-ID header is missing",
     }
 
-    response = client.simulate_post("/usage/email")
+    response = client.simulate_post(f"{PREFIX}/usage/email")
     assert response.status == "401 Unauthorized"
     assert response.json == {
         "error": "Unauthorized",
@@ -44,41 +46,61 @@ def test_usage_no_access_id(client):
 
 
 def test_email_limit(client):
-    response = client.simulate_get("/usage/email", headers={"Access-ID": "ananto"})
+    response = client.simulate_get(
+        f"{PREFIX}/usage/email", headers={"Access-ID": "ananto"}
+    )
     assert response.status == "200 OK"
     assert response.json == {"access_in_ms": 0}
 
-    response = client.simulate_post("/usage/email", headers={"Access-ID": "ananto"})
+    response = client.simulate_post(
+        f"{PREFIX}/usage/email", headers={"Access-ID": "ananto"}
+    )
     assert response.status == "201 Created"
-    response = client.simulate_post("/usage/email", headers={"Access-ID": "ananto"})
+    response = client.simulate_post(
+        f"{PREFIX}/usage/email", headers={"Access-ID": "ananto"}
+    )
     assert response.status == "201 Created"
 
-    response = client.simulate_get("/usage/email", headers={"Access-ID": "ananto"})
+    response = client.simulate_get(
+        f"{PREFIX}/usage/email", headers={"Access-ID": "ananto"}
+    )
     assert response.status == "200 OK"
     assert response.json["access_in_ms"] > 500
 
     time.sleep(1)
 
-    response = client.simulate_get("/usage/email", headers={"Access-ID": "ananto"})
+    response = client.simulate_get(
+        f"{PREFIX}/usage/email", headers={"Access-ID": "ananto"}
+    )
     assert response.status == "200 OK"
     assert response.json == {"access_in_ms": 0}
 
-    response = client.simulate_post("/usage/email", headers={"Access-ID": "ananto"})
+    response = client.simulate_post(
+        f"{PREFIX}/usage/email", headers={"Access-ID": "ananto"}
+    )
     assert response.status == "201 Created"
-    response = client.simulate_post("/usage/email", headers={"Access-ID": "ananto"})
+    response = client.simulate_post(
+        f"{PREFIX}/usage/email", headers={"Access-ID": "ananto"}
+    )
     assert response.status == "201 Created"
 
     # 4 times used within last 5 sec
-    response = client.simulate_get("/usage/email", headers={"Access-ID": "ananto"})
+    response = client.simulate_get(
+        f"{PREFIX}/usage/email", headers={"Access-ID": "ananto"}
+    )
     assert response.status == "200 OK"
     assert response.json["access_in_ms"] > 500
 
     time.sleep(1)
 
-    response = client.simulate_post("/usage/email", headers={"Access-ID": "ananto"})
+    response = client.simulate_post(
+        f"{PREFIX}/usage/email", headers={"Access-ID": "ananto"}
+    )
     assert response.status == "201 Created"
 
     # 5 times used within last 5 sec
-    response = client.simulate_get("/usage/email", headers={"Access-ID": "ananto"})
+    response = client.simulate_get(
+        f"{PREFIX}/usage/email", headers={"Access-ID": "ananto"}
+    )
     assert response.status == "200 OK"
     assert response.json["access_in_ms"] > 2500
